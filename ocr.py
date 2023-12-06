f82123d17469f92951af5e8a3ab1d5fa6131d3d6
Kathy <yuxuan.zh.jade@gmail.com>
2023-12-07 00:13:07 +0800

    changes

--- ocr.py
+++ ocr.py
@@ -1,13 +1,14 @@
 import keras_ocr
-import matplotlib.pyplot as plt
 
 # Get the pipeline from keras-ocr, which is a convenience wrapper that combines the detector and recognizer models into a single pipeline.
 pipeline = keras_ocr.pipeline.Pipeline()
 
 # Use the pipeline to recognize text from an image. You can use any image file format that is supported by keras (e.g., png, jpg).
-image = keras_ocr.tools.read('path_to_your_image.jpg') 
+image = keras_ocr.tools.read('test/1.png') 
 predictions = pipeline.recognize([image])[0]
 
-# Display the image and the predicted text
-fig, ax = plt.subplots()
-keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+for text, box in predictions:
+    print(text)
+
+recognized_text = ' '.join([text for text, box in predictions])
+print(recognized_text)
